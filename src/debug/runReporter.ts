b364a86acede65f350fa234e50124d9205c06342
Nuckyz <61953774+Nuckyz@users.noreply.github.com>
2026-08-11 22:48:56 -0300

    one more try

--- src/debug/runReporter.ts
+++ src/debug/runReporter.ts
@@ -32,7 +32,7 @@ async function runReporter() {
         Vencord.Webpack._initReporter = function () {
             // initReporter is called in the patched entry point of Discord
             // setImmediate to only start searching for lazy chunks after Discord initialized the app
-            setTimeout(() => loadLazyChunks().then(loadLazyChunksResolve), 0);
+            loadLazyChunks().then(loadLazyChunksResolve);
         };
 
         await loadLazyChunksDone;
